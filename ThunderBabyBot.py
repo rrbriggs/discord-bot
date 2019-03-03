8db5447d1f66ef7cb1c34ba2467c0399ae349abe
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-03-03 13:29:58 -0600

    add channel for the bot to report to

--- ThunderBabyBot.py
+++ ThunderBabyBot.py
@@ -135,14 +135,17 @@ async def raid_reminder():
     await client.wait_until_ready()
 
     channel = discord.Object(id=479104049325801483)
+    pub_channel = discord.Object(id=517037569377173504)
 
     while not client.is_closed:
         tardis = datetime.datetime.now()
         if tardis.weekday() == 4 and tardis.hour == 17:
             await client.send_message(channel, "@everyone RAID DAY: FRIDAY - 8:30PM CST (Texas Time/Freedom Time) THIS IS 9:30 EST/peon time")
+            await client.send_message(pub_channel, "@everyone RAID DAY: FRIDAY - 8:30PM CST (Texas Time/Freedom Time) THIS IS 9:30 EST/peon time")
 
         if tardis.weekday() == 5 and tardis.hour == 17:
             await client.send_message(channel, "@everyone RAID DAY: SATURDAY - 8:00PM CST (Texas Time/Freedom Time) THIS IS 9:00 EST/peon time")
+            await client.send_message(pub_channel, "@everyone RAID DAY: SATURDAY - 8:00PM CST (Texas Time/Freedom Time) THIS IS 9:00 EST/peon time")
 
         await asyncio.sleep(1800)
 
